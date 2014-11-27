9ced6dfc34538af39c22336fea968ed4b9cc1dde
Ramarao <ramarao.yarlagadda@yahoo.com>
2014-11-27 09:03:40 +0530

    Added logic to print numbers

--- Hello.java
+++ Hello.java
@@ -7,5 +7,11 @@ public class Hello
 		System.out.println("Testing Main method");
 		//Replacing print statement with log statement
 		System.out.println("Replacing print statement with log statement");
+		
+		// Adding logic to print numbers
+		for (int i = 0;i<10;i++)
+		{
+			System.out.print(i);
+		}
 	}
 }
